8742f236ace530d6ae3d05f0046cafe2d78087c0
Radstoria <radstoria@gmail.com>
2025-11-27 16:30:53 +0700

    Chore: apply .gitignore and cleanup tracked files

--- testDB.php
+++
@@ -1,8 +0,0 @@
-<?php
-$con = new mysqli("localhost","radstori_rainhub","Rags99Rkun1112Nyad","radstori_rainhub");
-
-if ($con->connect_errno) {
-    die("ERROR: ".$con->connect_error);
-}
-
-echo "CONNECTED!";
